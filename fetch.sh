e7611603d75d72eefc317c3f6ce08e66c3b7871b
karannayan <karan25991@gmail.com>
2014-09-10 15:54:29 +0530

    Auto update script added

--- fetch.sh
+++ fetch.sh
@@ -1,3 +1,20 @@
 #! /bin/bash
-i2cdetect -y 1
-echo "hello"
+echo "GIT UPDATE"
+echo "----------"
+cd /home/pi/Desktop/GoPiGo
+sudo git fetch origin
+sudo git merge origin/master
+
+cd Setup
+echo "UPDATING LIBRARIES"
+echo "------------------"
+sudo chmod +x install.sh
+sudo ./install.sh
+cd ../Firmware/
+
+echo "UPDATING FIRMWARE"
+echo "------------------"
+sudo chmod +x firmware_update.sh
+sudo ./firmware_update.sh
+echo "Please restart the Raspberry Pi for the changes to take effect"
+
