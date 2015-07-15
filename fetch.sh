a2ae4892d632d5809eb2d42f67a6e5423eb06103
Dexter Industries <dxtrindustries@gmail.com>
2015-07-15 13:16:04 +0400

    1

--- fetch.sh
+++ fetch.sh
@@ -1,21 +1,36 @@
 #! /bin/bash
-echo "GIT UPDATE"
+# This script updates the the code repos on Raspbian for Robots.
+echo "Start GoPiGo Update."
 echo "----------"
 cd /home/pi/Desktop/GoPiGo
 sudo git fetch origin
 git reset --hard
 sudo git merge origin/master
-
 cd Setup
 echo "UPDATING LIBRARIES"
 echo "------------------"
 sudo chmod +x install.sh
 sudo ./install.sh
 cd ../Firmware/
-
 echo "UPDATING FIRMWARE"
+echo "Remove motors from the GoPiGo."
 echo "------------------"
 sudo chmod +x firmware_update.sh
 sudo ./firmware_update.sh
-echo "Please restart the Raspberry Pi for the changes to take effect"
+
+# Rebooting!
+# We must reboot for folks.
+echo "Rebooting in 5 seconds!"
+sleep 1
+echo "Rebooting in 4 seconds!"
+sleep 1
+echo "Rebooting in 3 seconds!"
+sleep 1
+echo "Rebooting in 2 seconds!"
+sleep 1
+echo "Rebooting in 1 seconds!"
+sleep 1
+echo "Rebooting now!  Your Pi wake up with a freshly updated Raspberry Pi!"
+sleep 1
+sudo reboot
 
