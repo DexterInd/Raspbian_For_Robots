aec5d6bd461b7fbb2c45c85afb86e36d557fef04
Dexter Industries <dxtrindustries@gmail.com>
2015-07-16 21:57:58 +0400

    Added shellinabox, apache, php, and a startup website.
    Added shellinabox, apache, php.  A startup website that will allow the
    user to login and find these pages.

---
+++ www/index.php
@@ -0,0 +1,8 @@
+<html><body><h1>Welcome to Raspberry Pi!</h1>
+<p>This is the default web page for this server.</p>
+<p>The web server software is running but no content has been added, yet.</p>
+</body></html>
+
+<a href="http://raspberrypi.local:4200/"> Link for Shell Command Line. </a>
+<a href="http://raspberrypi.local:4200/"> Link for noVNC Desktop. </a>
+<a href="http://dexterindustries.com/forum"> Link for Dexter Industries Forums. </a>
