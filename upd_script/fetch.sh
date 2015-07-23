8bd259d8b5884c49dfb623c1f5afa4a992ebef77
Dexter Industries <dxtrindustries@gmail.com>
2015-07-23 16:35:52 +0400

    Update install scripts.

--- upd_script/fetch.sh
+++ upd_script/fetch.sh
@@ -2,39 +2,39 @@
 # This script updates the the code repos on Raspbian for Robots.
 
 # GoPiGo Update
-echo "Start GoPiGo Update."
+echo "--> Start GoPiGo Update."
 echo "----------"
 cd /home/pi/Desktop/GoPiGo
 sudo git fetch origin
 git reset --hard
 sudo git merge origin/master
 cd Setup
-echo "UPDATING LIBRARIES"
+echo "--> UPDATING LIBRARIES"
 echo "------------------"
 sudo chmod +x install.sh
 sudo ./install.sh
 cd ../Firmware/
-echo "UPDATING FIRMWARE"
-echo "Remove motors from the GoPiGo."
+echo "--> UPDATING FIRMWARE"
+echo "--> Remove motors from the GoPiGo."
 echo "------------------"
 sudo chmod +x firmware_update.sh
 sudo ./firmware_update.sh
 
-echo "Install Scratch dependency ScratchPy."
+echo "--> Install Scratch dependency ScratchPy."
 cd Desktop/GoPiGo/Software/Scratch
 sudo git clone https://github.com/DexterInd/scratchpy.git
 cd scratchpy
 sudo make install
 
 #GoPiGo Scratch Permissions
-#echo "Install Scratch Shortcuts and Permissions."
+#echo "--> Install Scratch Shortcuts and Permissions."
 #sudo rm /home/pi/Desktop/GoPiGo_Scratch_Start.desktop  					# Delete old icons off desktop
 #sudo cp /home/pi/Desktop/GoPiGo/Software/Scratch/GoPiGo_Scratch_Scripts/GoPiGo_Scratch_Start.desktop /home/pi/Desktop	# Move icons to desktop
 sudo chmod +x /home/pi/Desktop/GoPiGo/Software/Scratch/GoPiGo_Scratch_Scripts/GoPiGoScratch_debug.sh					# Change script permissions
 sudo chmod +x /home/pi/Desktop/GoPiGo/Software/Scratch/GoPiGo_Scratch_Scripts/GoPiGo_Scratch_Start.sh					# Change script permissions
 
 # BrickPi Update
-echo "Start BrickPi Update."
+echo "--> Start BrickPi Update."
 echo "----------"
 cd /home/pi/Desktop/BrickPi
 sudo git fetch origin
@@ -42,7 +42,7 @@ git reset --hard
 sudo git merge origin/master
 
 # BrickPi_Python Update
-echo "Start BrickPi_Python Update."
+echo "--> Start BrickPi_Python Update."
 echo "----------"
 cd /home/pi/Desktop/BrickPi_Python
 sudo git fetch origin
@@ -53,7 +53,7 @@ sudo apt-get install python-setuptools
 sudo python /home/pi/Desktop/BrickPi_Python/setup.py install
 
 # BrickPi_Scratch Update
-echo "Start BrickPi_Scratch Update."
+echo "--> Start BrickPi_Scratch Update."
 echo "----------"
 cd /home/pi/Desktop/BrickPi_Scratch
 sudo git fetch origin
@@ -71,7 +71,7 @@ sudo chmod +x BrickPi_Scratch_Start.sh
 sudo chmod +x BrickPiScratch_debug.sh
 
 # BrickPi_C Update
-echo "Start BrickPi_C Update."
+echo "--> Start BrickPi_C Update."
 echo "----------"
 cd /home/pi/Desktop/BrickPi_C
 sudo git fetch origin
@@ -79,7 +79,7 @@ git reset --hard
 sudo git merge origin/master
 
 # Arduberry Update
-echo "Start Arduberry Update."
+echo "--> Start Arduberry Update."
 echo "----------"
 cd /home/pi/Desktop/Arduberry
 sudo git fetch origin
@@ -90,7 +90,7 @@ sudo chmod +x install.sh
 sudo ./install.sh
 
 # GrovePi Update
-echo "Start GrovePi Update."
+echo "--> Start GrovePi Update."
 echo "----------"
 cd /home/pi/Desktop/GrovePi
 sudo git fetch origin
@@ -107,10 +107,11 @@ sudo chmod +x /home/pi/Desktop/GrovePi/Software/Scratch/GrovePi_Scratch_Scripts/
 sudo chmod +x /home/pi/Desktop/GrovePi/Software/Scratch/GrovePi_Scratch_Scripts/GrovePi_Scratch_Start.sh					# Change script permissions
 
 # Install DexterEd Software
+echo "--> Install DexterEd Software"
 cd /home/pi/Desktop
 sudo git pull https://github.com/DexterInd/DexterEd/
 chmod +x /home/pi/Desktop/DexterEd/Scratch_GUI/install_scratch_start.sh
 sudo ./home/pi/Desktop/DexterEd/Scratch_GUI/install_scratch_start.sh
 
 
-echo "Done updating Dexter Industries Github repos!"
+echo "--> Done updating Dexter Industries Github repos!"
