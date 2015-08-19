7b801272d7a632192df9ca495e8129a89801da99
Dexter Industries <dxtrindustries@gmail.com>
2015-08-19 11:31:21 +0400

    Update test again.

--- upd_script/upd.sh
+++ upd_script/upd.sh
@@ -1,5 +1,6 @@
 #! /bin/bash
 sudo rm -r /tmp/di_update
+echo "==========Start Upate!============"
 mkdir /tmp/di_update
 wget https://raw.githubusercontent.com/DexterInd/Raspbian_For_Robots/master/fetch.sh -O /tmp/di_update/update.sh
 chmod +x /tmp/di_update/update.sh
