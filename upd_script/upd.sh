f5e768a5f03ee087b03ee1b2e7738dcf2675ff95
Dexter Industries <dxtrindustries@gmail.com>
2015-08-19 11:21:28 +0400

    Add rm to remove old update temp files

--- upd_script/upd.sh
+++ upd_script/upd.sh
@@ -1,4 +1,5 @@
 #! /bin/bash
+sudo rm -r /tmp/di_update
 mkdir /tmp/di_update
 wget https://raw.githubusercontent.com/DexterInd/Raspbian_For_Robots/master/fetch.sh -O /tmp/di_update/update.sh
 chmod +x /tmp/di_update/update.sh
