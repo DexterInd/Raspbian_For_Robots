a53894c15fd2161cd184f3b3d23816f9fcb12d14
Dexter Industries <dxtrindustries@gmail.com>
2015-11-04 15:00:19 +0400

    Added the backup option

---
+++ update_backup.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+# Nuclear Backup Option.  
+
+sudo rm -r /home/pi/di_update
+
+mkdir /home/pi/di_update
+cd /home/pi/di_update
+sudo git clone https://github.com/DexterInd/Raspbian_For_Robots/
+cd Raspbian_For_Robots
+
+cd /home/pi/di_update/Raspbian_For_Robots/
+# git checkout update201507
+
+sudo chmod +x /home/pi/di_update/Raspbian_For_Robots/update_master.sh
+sudo chmod +x /home/pi/di_update/Raspbian_For_Robots/upd_script/update_all.sh
+sudo chmod +x /home/pi/di_update/Raspbian_For_Robots/raspbian_for_robots_update.py
+
+
