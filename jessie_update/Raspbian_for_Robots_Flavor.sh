79b3e04d8dbcd299afd058568ed86498b37c8a42
Nicole Parrot <cleoqc1124@gmail.com>
2016-03-10 10:14:42 -0500

    Create Raspbian_for_Robots_Flavor.sh
    This script contains the commands that are necessary to give Rasbpian its flavour. 
    TBD: change background icon

---
+++ jessie_update/Raspbian_for_Robots_Flavor.sh
@@ -0,0 +1,43 @@
+# This script sets up the environment to fit with Raspbian for Robots, but none of the actual Dexter Industries software
+
+# 1. User Name is Pi, Password is robots1234
+# 2. Hostname is dex
+# 3. Installing Samba
+
+DEFAULT_PWD=robots1234
+
+####################################
+# Changing Pi password to robots1234
+####################################
+echo pi:$DEFAULT_PWD > pipasswd
+sudo chpasswd < pipasswd
+
+####################################
+# set default hostname to dex
+####################################
+# Re-write /etc/hosts
+sed 's/raspberrypi/dex/g' </etc/hosts > hosts
+sudo cp hosts /etc/hosts
+rm hosts
+
+sudo rm /etc/hostname
+echo "Deleted hostname.  Create new hostname."
+sudo echo $NEW_HOST >> /etc/hostname
+echo "New hostname file created."
+
+echo "Commit hostname change."
+sudo /etc/init.d/hostname.sh
+
+####################################
+# Installing Samba
+####################################
+# samba is needed to gain access to the drive on Windows
+sudo apt-get install -y samba samba-common-bin
+echo #DEFAULT_PWD >> smbpasswd.txt
+echo #DEFAULT_PWD >> smbpasswd.txt
+
+sudo cp /etc/samba/smb.conf .
+sed '/s/read only = yes/read only = no/g' < smb.conf > smb.conf
+sudo cp smb.conf /etc/samba/smb.conf
+sudo service samba restart
+sudo smbpasswd -a pi < smbpasswd.txt
