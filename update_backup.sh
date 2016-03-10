340c8822a28a914a9efa14d66a72337e4bd5bb81
Dexter Industries <dxtrindustries@gmail.com>
2016-03-10 14:13:48 +0400

    Update update_backup.sh

--- update_backup.sh
+++ update_backup.sh
@@ -15,7 +15,7 @@ sudo chmod +x /home/pi/di_update/Raspbian_For_Robots/update_master.sh
 sudo chmod +x /home/pi/di_update/Raspbian_For_Robots/upd_script/update_all.sh
 sudo chmod +x /home/pi/di_update/Raspbian_For_Robots/raspbian_for_robots_update.py
 
-sudo apt-get install python-wxgtk2.8 python-wxtools wx2.8-i18n --force-yes			# Install wx for python for windows / GUI programs.
+sudo apt-get install python-wxgtk2.8 python-wxtools wx2.8-i18n --force-yes -y			# Install wx for python for windows / GUI programs.
 echo "Installed wxpython tools"
-sudo apt-get install python-psutil --force-yes
+sudo apt-get install python-psutil --force-yes -y
 echo "Python-PSUtil"
