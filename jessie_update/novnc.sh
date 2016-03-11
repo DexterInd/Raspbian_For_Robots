a708c6b1d655dbc93ab0b3eef167f5579f4e8630
Nicole Parrot <cleoqc1124@gmail.com>
2016-03-11 15:33:17 -0500

    Create novnc.sh
    first attempt at installing novnc

---
+++ jessie_update/novnc.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+# Install noVNC
+
+# cd into appropriate directory first.
+# I don't know where it's supposed to be
+cd /home/pi
+
+git clone git://github.com/kanaka/noVNC
+cd utils
