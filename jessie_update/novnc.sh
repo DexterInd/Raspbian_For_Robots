980e039843626d703238798b238b2d2ee130fe9f
Nicole Parrot <cleoqc1124@gmail.com>
2016-03-11 15:35:57 -0500

    Update novnc.sh
    Start novnc at boot

--- jessie_update/novnc.sh
+++ jessie_update/novnc.sh
@@ -7,4 +7,9 @@
 cd /home/pi
 
 git clone git://github.com/kanaka/noVNC
-cd utils
+
+cd /etc/systesmd/system
+sudo wget https://raw.githubusercontent.com/CleoQc/Raspbian_For_Robots/master/jessie_update/novnc.service
+sudo systemctl daemon-reload
+sudo systemctl enable novnc.service
+sudp systemctl start novnc.service
