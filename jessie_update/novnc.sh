ac6adf21864aa9c55a940812a2e70f4ba285482a
Nicole Parrot <cleoqc1124@gmail.com>
2016-03-11 22:06:58 -0500

    Update novnc.sh

--- jessie_update/novnc.sh
+++ jessie_update/novnc.sh
@@ -11,8 +11,9 @@ cd noVNC/utils
 chmod +x launch.sh
 ./launch.sh
 
-cd /etc/systesmd/system
+cd ~
 sudo wget https://raw.githubusercontent.com/CleoQc/Raspbian_For_Robots/master/jessie_update/novnc.service
+sudo cp novnc.service /etc/systemd/system/novnc.service
 sudo systemctl daemon-reload
 sudo systemctl enable novnc.service
 sudo systemctl start novnc.service
