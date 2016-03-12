17e4e0d7e23631bea319a80341269b217925fa31
Nicole Parrot <cleoqc1124@gmail.com>
2016-03-11 20:33:03 -0500

    Update novnc.sh

--- jessie_update/novnc.sh
+++ jessie_update/novnc.sh
@@ -7,6 +7,9 @@
 cd /home/pi
 
 git clone git://github.com/kanaka/noVNC
+cd utils
+chmod +x launch.sh
+./launch.sh
 
 cd /etc/systesmd/system
 sudo wget https://raw.githubusercontent.com/CleoQc/Raspbian_For_Robots/master/jessie_update/novnc.service
