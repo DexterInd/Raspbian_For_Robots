7f7b0efcb25244b917a2eac06c88f761d72f72a8
Nicole Parrot <cleoqc1124@gmail.com>
2016-03-11 22:11:58 -0500

    Update novnc.sh

--- jessie_update/novnc.sh
+++ jessie_update/novnc.sh
@@ -9,11 +9,12 @@ cd /home/pi
 git clone git://github.com/kanaka/noVNC
 cd noVNC/utils
 chmod +x launch.sh
-./launch.sh
+./launch.sh & # must be run in background otherwise the script blocks
 
 cd ~
 sudo wget https://raw.githubusercontent.com/CleoQc/Raspbian_For_Robots/master/jessie_update/novnc.service
 sudo cp novnc.service /etc/systemd/system/novnc.service
 sudo systemctl daemon-reload
 sudo systemctl enable novnc.service
-sudo systemctl start novnc.service
+sudo rm novnc.service
+# sudo systemctl start novnc.service # not needed
