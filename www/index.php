8a048fd1493358feabf644a191b24af1868cad06
Dexter Industries <dxtrindustries@gmail.com>
2016-03-18 14:20:13 +0400

    Eliminate cache storage.
    Eliminate cache storage on the index file for dex.local

--- www/index.php
+++ www/index.php
@@ -17,6 +17,11 @@
     <title>Dexter Industries Raspbian for Robots</title>
     <link rel="stylesheet" href="css/main.css">
     <meta name="viewport" content="width=device-width,initial-scale=1">
+    <meta http-equiv="cache-control" content="max-age=0" />
+    <meta http-equiv="cache-control" content="no-cache" />
+    <meta http-equiv="expires" content="0" />
+    <meta http-equiv="expires" content="Tue, 01 Jan 1980 1:00:00 GMT" />
+    <meta http-equiv="pragma" content="no-cache" />
 </head>
 
 <body>
