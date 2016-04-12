afcbf291fd50d7029abaad6a71cf4a238ccb8028
Karan Nayan <karan25991@gmail.com>
2016-04-12 11:51:02 +0530

    backup script bugs removed

--- backup/call_restore.sh
+++ backup/call_restore.sh
@@ -3,4 +3,4 @@
 
 today=`date '+%Y_%m_%d__%H_%M_%S'`;
 filename="/home/pi/Desktop/Restore_Script_Log_$today.txt" 
-script -c 'sudo sh /home/pi/di_update/Raspbian_For_Robots/backup/restore.sh 2>&1' -f $filename
+script -c 'sudo bash /home/pi/di_update/Raspbian_For_Robots/backup/restore.sh 2>&1' -f $filename
