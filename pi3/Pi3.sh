ae3cc2f54bd6b4ebdbe9ed45916bb0986aad039b
Karan Nayan <karan25991@gmail.com>
2016-04-22 15:11:58 +0530

    Overlays for Jessie added

--- pi3/Pi3.sh
+++ pi3/Pi3.sh
@@ -7,15 +7,26 @@
 #
 # Note this is called from upd_script/update_all.sh 
 
-sudo apt-get update -y
-sudo apt-get dist-upgrade -y
-sudo rpi-update -y
+# sudo apt-get update -y
+# sudo apt-get dist-upgrade -y
+# sudo rpi-update -y
 
 cd /home/pi/
-sudo wget https://github.com/DexterInd/Raspbian_For_Robots/raw/master/pi3/pi3-miniuart-bt-overlay.dtb
-sudo cp pi3-miniuart-bt-overlay.dtb /boot/overlays
 
-sudo sed -i "/dtoverlay=pi3-miniuart-bt-overlay/d" /boot/config.txt
-sudo sed -i "/force_turbo=1/d" /boot/config.txt
-sudo echo "dtoverlay=pi3-miniuart-bt-overlay" >> /boot/config.txt
-sudo echo "force_turbo=1" >> /boot/config.txt
+if grep -q "VERSION_ID=\"8\"" /etc/os-release
+then
+    #os is Jessie
+    sudo sed -i "/dtoverlay=pi3-miniuart-bt-overlay/d" /boot/config.txt
+    sudo sed -i "/force_turbo=1/d" /boot/config.txt
+    sudo sed -i "/dtoverlay=pi3-miniuart-bt/d" /boot/config.txt
+    sudo echo "dtoverlay=pi3-miniuart-bt" >> /boot/config.txt
+else
+    #os is wheezy
+    sudo wget https://github.com/DexterInd/Raspbian_For_Robots/raw/master/pi3/pi3-miniuart-bt-overlay.dtb
+    sudo cp pi3-miniuart-bt-overlay.dtb /boot/overlays
+    sudo sed -i "/dtoverlay=pi3-miniuart-bt-overlay/d" /boot/config.txt
+    sudo sed -i "/force_turbo=1/d" /boot/config.txt
+    sudo sed -i "/dtoverlay=pi3-miniuart-bt/d" /boot/config.txt
+    sudo echo "dtoverlay=pi3-miniuart-bt-overlay" >> /boot/config.txt
+    sudo echo "force_turbo=1" >> /boot/config.txt
+fi
