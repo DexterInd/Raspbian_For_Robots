94adc7373eb1797b01e3700b2317c39b840063f4
Dexter Industries <dxtrindustries@gmail.com>
2016-10-10 21:12:13 +0400

    Change pins on GPIO for SpyvsSpy
    Originally, the pins 7/8 don't work: they are pulled high by the mysterious linux devicetree settings on startup.  These pins are not pulled up; they work.

--- upd_script/spivsspi/SpyvsSpy_Startup.py
+++ upd_script/spivsspi/SpyvsSpy_Startup.py
@@ -4,8 +4,9 @@ from time import sleep		# Allows us to call the sleep function to slow down our 
 import RPi.GPIO as GPIO		# Allows us to call our GPIO pins and names it just GPIO
    
 GPIO.setmode(GPIO.BCM)		# Set's GPIO pins to BCM GPIO numbering
-INPUT_PIN_0 = 7				# GPI07 (26)
-INPUT_PIN_1 = 8				# GPIO8 (24)
+INPUT_PIN_0 = 9				# GPI09
+INPUT_PIN_1 = 11			# GPIO11
+
 GPIO.setup(INPUT_PIN_0, GPIO.IN)           # Set our input pin to be an input
 GPIO.setup(INPUT_PIN_1, GPIO.IN)           # Set our input pin to be an input
 
