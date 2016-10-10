41fd3c56afd82f6c9eaedaa840767fbe70b39229
Dexter Industries <dxtrindustries@gmail.com>
2016-10-10 12:07:54 +0400

    Update for manual run.

--- jessie_update/tightvncserver.sh
+++ jessie_update/tightvncserver.sh
@@ -4,20 +4,20 @@
 ####################################
 
 # get default password from the argument
-DEFAULT_PWD=$1
+#DEFAULT_PWD=$1
 
-sudo apt-get install tightvncserver expect -y
-/usr/bin/expect <<EOF
-spawn "/usr/bin/tightvncserver"
-expect "Password:"
-send "$DEFAULT_PWD\r"
-expect "Verify:"
-send "$DEFAULT_PWD\r"
-expect "(y/n?"
-send "n\r"
-expect eof
-EOF
-sudo apt-get remove expect -y
+#sudo apt-get install tightvncserver expect -y
+#/usr/bin/expect <<EOF
+#spawn "/usr/bin/tightvncserver"
+#expect "Password:"
+#send "$DEFAULT_PWD\r"
+#expect "Verify:"
+#send "$DEFAULT_PWD\r"
+#expect "(y/n?"
+#send "n\r"
+#expect eof
+#EOF
+#sudo apt-get remove expect -y
 
 # change cursor
 sed -i 's/grey/grey -cursor_name left_ptr/g'  ./.vnc/xstartup 
