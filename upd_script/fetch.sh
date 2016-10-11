4744541b3d7db2d254e52dc182a4674f8f813d9a
Dexter Industries <dxtrindustries@gmail.com>
2016-10-11 14:17:28 +0400

    Moving Spy vs Spi

--- upd_script/fetch.sh
+++ upd_script/fetch.sh
@@ -177,10 +177,6 @@ if [ $grovepi_update == 1 ] ; then
 	sudo chmod +x /home/pi/Desktop/GrovePi/Software/Scratch/GrovePi_Scratch_Scripts/GrovePiScratch_debug.sh						# Change script permissions
 	sudo chmod +x /home/pi/Desktop/GrovePi/Software/Scratch/GrovePi_Scratch_Scripts/GrovePi_Scratch_Start.sh					# Change script permissions
 	
-	# Install Spy vs sPi Startup.
-	sudo chmod +x /home/pi/di_update/Raspbian_For_Robots/upd_script/spivsspi/SpyVsSpy_install.sh
-	sudo sh /home/pi/di_update/Raspbian_For_Robots/upd_script/spivsspi/SpyVsSpy_install.sh
-	
 else
 	echo "--> GrovePi **NOT** Updated."
 	echo "----------"
