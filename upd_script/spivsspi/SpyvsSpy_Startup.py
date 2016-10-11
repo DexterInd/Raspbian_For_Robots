c4ec925c922da0cc2d2c579681175745cab52d94
Dexter Industries <dxtrindustries@gmail.com>
2016-10-11 13:58:30 +0400

    GPIO Cleanup installed.
    On exit, cleanup the GPIO's.

--- upd_script/spivsspi/SpyvsSpy_Startup.py
+++ upd_script/spivsspi/SpyvsSpy_Startup.py
@@ -5,16 +5,21 @@ import RPi.GPIO as GPIO		# Allows us to call our GPIO pins and names it just GPI
 
 sleep(30)
 
-GPIO.setmode(GPIO.BCM)		# Set's GPIO pins to BCM GPIO numbering
-INPUT_PIN_0 = 9				# GPI09
-INPUT_PIN_1 = 11			# GPIO11
+try:
+	GPIO.setmode(GPIO.BCM)		# Set's GPIO pins to BCM GPIO numbering
+	INPUT_PIN_0 = 9				# GPI09
+	INPUT_PIN_1 = 11			# GPIO11
+	
+	GPIO.setup(INPUT_PIN_0, GPIO.IN)           # Set our input pin to be an input
+	GPIO.setup(INPUT_PIN_1, GPIO.IN)           # Set our input pin to be an input
+	
+	if((GPIO.input(INPUT_PIN_0) == True) and (GPIO.input(INPUT_PIN_1) == True)):
+		print('True!')
+		os.system("sudo python /home/pi/Mission1_pythonFile.py 1")
+	else:
+		print('False!')
+except:
+	print "Error!"
 
-GPIO.setup(INPUT_PIN_0, GPIO.IN)           # Set our input pin to be an input
-GPIO.setup(INPUT_PIN_1, GPIO.IN)           # Set our input pin to be an input
-
-
-if((GPIO.input(INPUT_PIN_0) == True) and (GPIO.input(INPUT_PIN_1) == True)):
-	print('True!')
-	os.system("sudo python /home/pi/Mission1_pythonFile.py 1")
-else:
-	print('False!')
+finally:
+	GPIO.cleanup()		# Clean exit, release the GPIO's.		
