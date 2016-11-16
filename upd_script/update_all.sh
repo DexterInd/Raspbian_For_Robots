625027905131645954c9ee5552dd57284b07f426
cleoqc <cleoqc1124@gmail.com>
2016-11-16 13:59:44 +0000

    add espeak

--- upd_script/update_all.sh
+++ upd_script/update_all.sh
@@ -48,7 +48,7 @@ sudo pip install -U future # for Python 2/3 compatibility
 
 echo "geany, espeak and piclone"
 # new tools from the foundation
-sudo apt-get install geany espeak  piclone-y
+sudo apt-get install geany espeak  piclone -y
 sudo sed -i '/^Exec/ c Exec=sudo geany %F' /usr/share/raspi-ui-overrides/applications/geany.desktop
 
 
