fce45183966b677ec836a43ddecfb7944f7752fd
cleoqc <cleoqc1124@mail.com>
2017-01-04 15:06:35 +0000

    made install_copypaste() more robust

--- upd_script/update_all.sh
+++ upd_script/update_all.sh
@@ -27,53 +27,28 @@ DEXTER=Dexter
 SCRATCH=Scratch_GUI
 SCRATCH_PATH=$HOME/$DEXTER/$SCRATCH
 
-feedback() {
-  # first parameter is text to be displayed
-  # this sets the text color to a pinkish color for visibility
-  # the last tput resets colors to default
-  # one could also set background color with setb instead of setaf
-  #http://www.tldp.org/HOWTO/Bash-Prompt-HOWTO/x405.html
-  echo -e "$(tput setaf 3)$1$(tput sgr0)"
-}
-
-delete_line_from_file() {
-  # first parameter is the string to be matched 
-  # the lines that contain that string will get deleted
-  # second parameter is the filename
-  if [ -f $2 ]
-  then
-    sudo sed -i "/$1/d" $2
-    feedback "deleted $1 from $2"
-  fi
-}
-
-insert_before_line_in_file()
-{
-  # first argument is the line that needs to be inserted DO NOT USE PATHS WITH / in them
-  # second argument is a partial match of the line we need to find to insert before
-  # third arument is filename
-
-  feedback "Inserting $1 before $2 in $3"
-  if [ -f $3 ]
-  then
-    feedback "sudo sed -i '/$2/i $1' $3"
-    sudo sed -i "/$2/i $1" $3
-  fi
-
-}
-add_line_to_end_of_file() {
-  # first parameter is what to add
-  # second parameter is filename
-  if [ -f $2 ]
-  then
-    echo $1 >> $2
-    feedback "added $1 in $2"
-  fi 
-}
+source ./functions_lib.sh
 
 install_copypaste() {
   # put "autocutsel -fork" before the last line in .vnc/xstartup
-  insert_before_line_in_file "autocutsel -fork" Xsession /home/pi/.vnc/xstartup
+  xstartup_file='xstartup'
+  vnc_dir='/home/pi/.vnc'
+  new_str='autocutsel -fork'
+  xstartup=$vnc_dir/$xstartup_file
+
+  if file_does_not_exists $xstartup
+  then
+    touch $xstartup
+    add_line_to_end_of_file "xrdb $HOME/.Xresources" $xstartup
+    add_line_to_end_of_file "xsetroot -solid grey -cursor_name left_ptr" $xstartup
+    add_line_to_end_of_file "/etc/X11/Xsession" $xstartup
+  fi
+
+  if ! find_in_file "$new_str" $xstartup
+    then
+      feedback "Installing copy/paste capabilities"
+      insert_before_line_in_file "$new_str" Xsession $xstartup
+  fi
 }
 
 feedback "--> Begin Update."
