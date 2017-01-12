e4092422d30b788505022d3b7560ccc0c2e20da2
Karan Nayan <karan25991@gmail.com>
2017-01-12 15:35:16 +0530

    GoPiGo and GrovePi f/w upd called via functions

--- upd_script/update_GoPiGo_Firmware.sh
+++ upd_script/update_GoPiGo_Firmware.sh
@@ -53,11 +53,8 @@ echo "0"
 sleep 1
 '''
 
-sudo avrdude -c gpio -p m328p -U lfuse:w:0x7F:m
-sudo avrdude -c gpio -p m328p -U hfuse:w:0xDA:m
-sudo avrdude -c gpio -p m328p -U efuse:w:0x05:m
-# avrdude -c gpio -p m328p -U flash:w:/home/pi/Desktop/GoPiGo/Firmware/fw_ver_13.cpp.hex
-sudo avrdude -c gpio -p m328p -U flash:w:/home/pi/Desktop/GoPiGo/Firmware/fw_ver_16/fw_ver_16.cpp.hex
+source /home/pi/Desktop/GoPiGo/Firmware/gopigo_firmware_update.sh
+update_gopigo_firmware
 
 echo "GoPiGo Firmware updated succesfully."
 echo "GoPiGo Firmware complete!"
