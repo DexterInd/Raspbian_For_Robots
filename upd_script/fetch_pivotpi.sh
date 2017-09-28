a7ed3e6e9807e18b1453d09aca6b8c225b35a19a
Robert Lucian Chiriac <robert.lucian.chiriac@gmail.com>
2017-09-28 12:53:04 +0300

    ref RobertLucian/Raspbian_For_Robots#9 : feature - removed fetchers (#160)

--- upd_script/fetch_pivotpi.sh
+++ upd_script/fetch_pivotpi.sh
@@ -1,25 +1,3 @@
-PIHOME=/home/pi
-DEXTER=Dexter
-DEXTER_PATH=$PIHOME/$DEXTER
-RASPBIAN=$PIHOME/di_update/Raspbian_For_Robots
-curl --silent https://raw.githubusercontent.com/DexterInd/script_tools/master/install_script_tools.sh | bash
-
-# needs to be sourced from here when we call this as a standalone
-source /home/pi/$DEXTER/lib/$DEXTER/script_tools/functions_library.sh
-
-pushd $DEXTER_PATH > /dev/null
-
-# if pivotpi folder doesn't exit then clone repo
-if [ ! -d "PivotPi" ]; then
-    sudo git clone https://github.com/DexterInd/PivotPi.git
-else
-    cd $DEXTER_PATH/PivotPi
-    sudo git fetch origin
-    sudo git reset --hard
-    sudo git merge origin/master
-fi
-#change_branch $BRANCH
-
-sudo bash $DEXTER_PATH/PivotPi/Install/install.sh
-
-popd > /dev/null
+# this file has been left in this place for legacy reasons
+# when we're sure the fetcher works on the corresponding repo, then we'll remove this file
+sudo curl -kL dexterindustries.com/update_pivotpi | bash
