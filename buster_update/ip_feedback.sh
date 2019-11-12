034c79f74080d0a899fd5d4c4c8959bf6b76f246
CleoQc <cleoqc1124@gmail.com>
2019-11-12 10:26:39 -0500

    install new network config approach

---
+++ buster_update/ip_feedback.sh
@@ -0,0 +1,47 @@
+echo "starting"
+COUNT=0
+IPs=$(hostname --all-ip-addresses)
+while [ -z "$IPs" ]
+do
+    echo "loop"
+    sleep 1;
+    IPs=$(hostname --all-ip-addresses)
+    COUNT=$((COUNT+1))
+    echo "count: "$COUNT > /home/pi/ipcount
+done
+
+echo "done looping "
+
+echo "count: "$COUNT > /home/pi/ipcount
+
+ifconfig wlan0 | grep 'inet ' | awk '{print $2}'  > /home/pi/ip.number
+read -r IP_NUMBER < /home/pi/ip.number
+echo $IP_NUMBER
+
+# remove previous IP info
+sudo rm /boot/*.assigned_ip &>/dev/null
+sudo rm /home/pi/Desktop/*.assigned_ip &>/dev/null
+
+# remove previous Failed IP
+sudo rm /home/pi/failedIP &>/dev/null
+sudo rm /home/pi/Desktop/failedIP &>/dev/null
+
+if [ ! -z "$IP_NUMBER" ]
+then
+        echo "saving IP info"
+        sudo bash -c "echo $IP_NUMBER > /boot/$IP_NUMBER.assigned_ip"
+        echo $IP_NUMBER > /home/pi/Desktop/$IP_NUMBER.assigned_ip
+        echo "IP info saved"
+
+        espeak-ng "WiFi IP"
+        espeak-ng $IP_NUMBER
+        espeak-ng " repeating "
+        espeak-ng $IP_NUMBER
+else
+#       espeak-ng "no IP number"
+        echo "no IP number"
+        echo "no IP" > /home/pi/failedIP
+        echo "no IP" > /home/pi/Desktop/failedIP
+
+fi
+echo "done with IP feedback"
